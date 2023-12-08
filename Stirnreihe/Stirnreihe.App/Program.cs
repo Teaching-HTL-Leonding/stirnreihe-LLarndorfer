5ce6ba2751b15be7860bf2ccc2bea2c03a21236f
LLarndorfer <116261839+LLarndorfer@users.noreply.github.com>
2023-12-08 21:40:52 +0100

    Add files via upload
    added one or two comments to explain, why I did something

---
+++ Stirnreihe/Stirnreihe.App/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Concurrent;
+using System.ComponentModel.DataAnnotations;
+using Stirnreihe.Data;
+string firstname;
+string lastname;
+int height;
+
+Console.WriteLine("Welcome to the Stirnreihe World Record App! What do you want to do? \n1) Add a person to the line \n1b) Add a person to the line (sorted) \n2) Print the line \n3) Clear the line \n4) Exit");
+var lineofpeople = new LineOfPeople(null);
+while (true)
+{
+    Console.Write("Your choice: ");
+    string userinput = Console.ReadLine()!;
+
+    switch (userinput)
+    {
+        case "1":
+
+            Console.Write($"First name: ");
+            firstname = Console.ReadLine()!;
+            Console.Write($"Last Name: ");
+            lastname = Console.ReadLine()!;
+            Console.Write($"Height in cm: ");
+            height = int.Parse(Console.ReadLine()!);
+            Person? person = new Person(firstname, lastname, height);
+            lineofpeople.AddToFront(person);
+            break;
+
+        case "1b":
+            Console.Write($"First name: ");
+            firstname = Console.ReadLine()!;
+            Console.Write($"Last Name: ");
+            lastname = Console.ReadLine()!;
+            Console.Write($"Height in cm: ");
+            height = int.Parse(Console.ReadLine()!);
+            Person? sortedperson = new Person(firstname, lastname, height);
+            int index = lineofpeople.AddSorted(sortedperson);
+            System.Console.WriteLine($"{sortedperson.FirstName} lined up at index {index}");
+            break;
+
+        case "2":
+            for (var current = lineofpeople.First; current != null; current = current.next)
+            {
+                Console.WriteLine(current.person);
+            }
+
+            break;
+
+        case "3":
+            lineofpeople.Clear();
+            break;
+
+
+        case "4":
+            throw new ArgumentException("Exit pressed");
+    }
+}
---
+++ Stirnreihe/Stirnreihe.Data/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System.Globalization;
+using System.IO.Compression;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
+
+namespace Stirnreihe.Data;
+
+public class Person(string firstName, string lastName, int height)
+{
+    public string? FirstName { get; set; } = firstName;
+    public string? LastName { get; set; } = lastName;
+    public int? Height { get; set; } = height;
+
+    public override string ToString()
+    {
+        return $"{LastName}, {FirstName} ({Height} cm)";
+    }
+}
+
+
+public class Node(Person person, Node? next)
+{
+    public Person person { get; set; } = person;
+    public Node? next { get; set; } = next;
+}
+
+public class LineOfPeople(Node? First)
+{
+    public Node? First { get; set; } = First;
+    public void AddToFront(Person person)
+    {
+
+        var node = new Node(person, First);
+        // if(First != null)
+        // {
+        // First.next = First;
+        // }
+        First = node;
+
+
+    }
+
+    public int AddSorted(Person person)
+    {
+        
+
+        var counter = 1;
+        if (First == null || person.Height < First.person.Height)
+        {
+            AddToFront(person);
+            return 0;
+        }
+
+        Node current = First; // Außerhalb die Deklaration
+
+        for (; current.next != null; current = current.next)
+        {
+
+            if (person.Height < current.next.person.Height) // Durch <, statt <= geht er bei gleichen Größen durch bis entweder jemand größeres current.next ist, oder bis current.next null ist. Dadurch reiht sich eine gleiche Person, mit der gleichen Höhe hinten ein.
+            {
+                break;
+            }
+            counter++;
+        }
+
+
+        var sortednode = new Node(person, current.next);
+        current.next = sortednode;
+        return counter;    
+        // current = sortednode;
+    }
+
+
+    public void Clear()
+    {
+        First = null;
+    }
+}
